d962756fc6b24ed4f99885e8968cdb803756597e
Samandar <samandarx6ja@gmail.com>
2022-06-28 13:09:09 +0500

    home header 2/1 respon section

--- src/component/home/Homeheader.js
+++ src/component/home/Homeheader.js
@@ -3,19 +3,9 @@ import { Link } from 'react-router-dom'
 
 const HomeFooter = () => {
   return (
-    <div className="home__header">
-      <Link to="/">
-        <div className="home__headlink">
-          <h1>Держим в курсе</h1>
-          <p>Что сейчас делать: актуальные новости, советы, инструкции</p>
-          <span>
-            {' '}
-            <p> Перейти к разделу</p>
-          </span>
-        </div>
-      </Link>
-      <div className="home_item">
-        <Link>
+    <div className="container__max">
+      <div className="home__header">
+        <Link to="/">
           <div className="home__headlink">
             <h1>Держим в курсе</h1>
             <p>Что сейчас делать: актуальные новости, советы, инструкции</p>
@@ -25,13 +15,25 @@ const HomeFooter = () => {
             </span>
           </div>
         </Link>
-        <Link>
-          <div className="home__headlink">
-            <h1>Держим в курсе</h1>
-            <p>Что сейчас делать: актуальные новости, советы, инструкции</p>
+      </div>
+      <div className="home__item">
+        <Link to="/">
+          <div className="home__banner banner1">
+            <h1>Сервер для бизнеса</h1>
+            <p> Обслуживаем, оптимизируем и мониторим 24/7!</p>
             <span>
               {' '}
-              <p> Перейти к разделу</p>
+              <p>Больше об услуге</p>
+            </span>
+          </div>
+        </Link>
+        <Link to="/">
+          <div className="home__banner banner2">
+            <h1>Суперцена на .XYZ</h1>
+            <p>Теперь всего 85 рублей!</p>
+            <span>
+              {' '}
+              <p> Выбрать домен</p>
             </span>
           </div>
         </Link>
